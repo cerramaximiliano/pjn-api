b40e326e8bc126c6c6939247fd6623edf8741fd4
Maximiliano Cerra <cerramaximiliano@gmail.com>
2025-06-24 20:32:00 -0300

    fix/ ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
         {
             name: "pjn/api",
             script: "src/server.js",
-            cwd: "/var/www/pjn-api",
             exec_mode: "fork", 
             instances: 1,
             autorestart: true,
